69d6fa509f96cbeb5ec8c9866d6502ad45a66042
Miguel <milo2202@gmail.com>
2016-09-26 20:56:52 -0400

    This is the role add.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -3,10 +3,48 @@ import proyecto_final.Categoria
 import proyecto_final.Juego
 import WebService.RespuestaActividad
 import proyecto_final.RegistroRespuestaAct
+import proyecto_final.Role
+import proyecto_final.User
+import proyecto_final.UserRole
 
 class BootStrap {
 
     def init = { servletContext ->
+        def rolePadre = new Role()
+        def roleProfesor = new Role()
+        def roleEstudiante = new Role()
+        def roleAdmin = new Role()
+
+        if (!Role.exists(Role.findByAuthority("PADRE"))) {
+            rolePadre.authority = "PADRE"
+            rolePadre.save(flush: true, failOnError: true)
+        }
+
+        if (!Role.exists(Role.findByAuthority("PROFESOR"))) {
+            roleProfesor.authority = "PROFESOR"
+            roleProfesor.save(flush: true, failOnError: true)
+        }
+        if (!Role.exists(Role.findByAuthority("ESTUDIANTE"))){
+            roleEstudiante.authority = "ESTUDIANTE"
+            roleEstudiante.save(flush: true, failOnError: true)
+        }
+
+        if (!Role.exists(Role.findByAuthority("ADMIN"))) {
+            roleAdmin.authority = "ADMIN"
+            roleAdmin.save(flush: true, failOnError: true)
+        }
+
+        def adminUser = new User()
+        adminUser.username = "admin"
+        adminUser.password = "admin"
+        adminUser.save(flush: true, failOnError: true)
+
+        try {
+            UserRole.create(adminUser, roleAdmin)
+        }catch (Exception e){
+            print(e.getMessage())
+        }
+
         Date newDate = new Date();
 
         Categoria categoria = new Categoria();
@@ -37,10 +75,10 @@ class BootStrap {
         ju.actividad = actividad;
         ju.save(flush: true, failOnError: true);
 
-        //actividad.addToRespuestasActividad(respuesta);
-        actividad.save();
+        actividad.addToRespuestaActividad(respuesta);
+        actividad.save(flush: true,failOnError: true);
 
-        RegistroRespuestaAct.create(actividad,respuesta);
+        //RegistroRespuestaAct.create(actividad,respuesta);
         println("Se creo...")
 
     }
